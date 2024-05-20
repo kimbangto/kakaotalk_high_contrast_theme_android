c31670c8d00db43ae2a87fbe84c341230e4127e4
kimbangto <kimchaeyeon.525@gmail.com>
2024-05-20 21:04:06 +0900

    카카오톡 고대비 안드로이드 테마

---
+++ build/generated/source/buildConfig/release/com/kakao/talk/theme/apeach/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kakao.talk.theme.apeach;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.kakao.talk.theme.template";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 101;
+  public static final String VERSION_NAME = "10.5.0";
+}
